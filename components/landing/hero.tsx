45d6baa6588fdedf2e4a0cd741aef6936a75b851
Lakshay Khattar <117038630+Lakszy@users.noreply.github.com>
2023-10-29 20:06:37 +0530

    Fixes: #1 The Visibility Issue
    Issue: On the landing page *Ultimate AI* was not visible properly, 
    The same goes for the text above and below the heading

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -24,12 +24,12 @@ const Hero = () => {
           </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="rounded-full px-3 py-1 text-sm leading-6 text-gray-600 border">
+              <div className="rounded-full px-3 py-1 text-sm leading-6 text-gray-400 border">
                 Brainfast - Unleash the Power of AI
               </div>
             </div>
             <div className="text-center">
-              <h1 className="text-4xl leading-8 font-bold tracking-tight text-gray-900 sm:text-6xl">
+              <h1 className="text-4xl leading-8 font-bold tracking-tight text-white sm:text-6xl">
                 Ultimate AI
                 <span>
                   <Typewriter
@@ -41,16 +41,16 @@ const Hero = () => {
                   />
                 </span>
               </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
+              <p className="mt-6 text-lg leading-8 text-gray-300">
                 All-in-one platform to generate AI content and start making money in minutes.
               </p>
               <div className="mt-10 lg:flex items-center justify-center gap-x-6">
                 <Link href="/dashboard">
-                  <Button className="gradient-btn w-full lg:w-auto mb-8 lg:mb-0" size="lg">
+                  <Button className=" text-white gradient-btn w-full lg:w-auto mb-8 lg:mb-0" size="lg">
                     Try Brainfast Free
                   </Button>
                 </Link>
-                <a href="#features" className="text-sm font-semibold leading-6 text-gray-900">
+                <a href="#features" className="text-sm font-semibold leading-6 text-gray-400">
                   Discover Brainfast AI <span aria-hidden="true">→</span>
                 </a>
               </div>
@@ -75,3 +75,4 @@ const Hero = () => {
 }
 
 export default Hero;
+
